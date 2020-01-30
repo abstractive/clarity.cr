5ce8384bdf5ce26e31e95f728c0925396b404d47
//de <code@extremist.digital>
2020-01-30 15:06:49 -0800

    remove complexity for return on Clarity.from_msgpack

--- src/clarity/conversions.cr
+++ src/clarity/conversions.cr
@@ -2,10 +2,6 @@ require "../clarity"
 
 def Clarity.from_msgpack(input)
   dismantle Hash(String, MessagePack::Type).from_msgpack(input)
-rescue MessagePack::TypeCastError
-  Gnosis.warn("Failed to generate and dismantle Hash; trying Array")
-  Gnosis.debug(input, "Clarity.from_msgpack")
-  dismantle Array(MessagePack::Type).from_msgpack(input)
 end
 
 def Clarity.to_msgpack(data)
