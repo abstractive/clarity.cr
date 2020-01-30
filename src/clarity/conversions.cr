7da90c847eb8c4efbad4575e36827bedb5f12478
//de <code@extremist.digital>
2020-01-30 15:05:57 -0800

    debug output for failure case

--- src/clarity/conversions.cr
+++ src/clarity/conversions.cr
@@ -3,6 +3,8 @@ require "../clarity"
 def Clarity.from_msgpack(input)
   dismantle Hash(String, MessagePack::Type).from_msgpack(input)
 rescue MessagePack::TypeCastError
+  Gnosis.warn("Failed to generate and dismantle Hash; trying Array")
+  Gnosis.debug(input, "Clarity.from_msgpack")
   dismantle Array(MessagePack::Type).from_msgpack(input)
 end
 
